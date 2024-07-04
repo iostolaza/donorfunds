e6e5e9254e0d89d6f2e87e0446675f9d092890fb
Francisco <i.ostolaza87@gmail.com>
2024-07-03 19:11:02 -0700

    Initial Commit

---
+++ donorfunds/displaymenu.hpp
@@ -0,0 +1,47 @@
+//
+//  displaymenu.hpp
+//  donorfunds
+//
+//  Created by Francisco Ostolaza on 6/28/24.
+//
+
+#ifndef displaymenu_hpp
+#define displaymenu_hpp
+
+#include "donorfunds.hpp"
+  
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Main menu functions
+void displayMainMenu();
+void displaySignInMenu();
+void displayTaskMenu();
+void displayTimesheetTaskMenu();
+void displayAccountTaskMenu();
+void displayReportMenu();
+
+// Employee management functions
+void manageTimesheet(Employee* employee);
+void addEmployeeHours(Employee* employee);
+void subtractEmployeeHours(Employee* employee);
+void showEmployeeHours(Employee* employee);
+void showEmployeeTotalPay(Employee* employee);
+void addEmployee(EmployeeManager& manager);
+void signInEmployee(EmployeeManager& manager);
+
+// Account management functions
+void manageAccounts(LinkedListAccounts* accounts);
+void addAccountFunds(LinkedListAccounts* accounts);
+void subtractAccountFunds(LinkedListAccounts* accounts);
+void viewAccountBal(LinkedListAccounts* accounts);
+void viewAccountsBal(LinkedListAccounts* accounts);
+
+// Report functions
+void createReport();
+
+
+#endif /* displaymenu_hpp */
+

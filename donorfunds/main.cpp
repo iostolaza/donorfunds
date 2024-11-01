6ade364a71663f0989439cb1a59853984444d809
Francisco <i.ostolaza87@gmail.com>
2024-11-01 18:29:20 +0700

    Changes: Implemented better functionality for account creation and deletion. Created a method to view account details. Pending: Need to streamline userprompts. Need to remove redundencies for viewing account details. Need to work on reporting. Need to work on timesheet management. Link timesheet with accounts.

--- donorfunds/main.cpp
+++ donorfunds/main.cpp
@@ -32,7 +32,7 @@ int main() {
                 cout << "Exiting program." << endl;
                 return 0;
             default:
-                cout << "Invalid choice! Please try again." << endl;
+                cout << "Invalid choice! Please try again. " << endl;
         }
     }
     
